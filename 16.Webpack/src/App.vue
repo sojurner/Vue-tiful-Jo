f8d61752a69c0a69c49d43f1dfbfd3ddc23dff01
sojurner <pykim0591@gmail.com>
2018-11-25 06:39:09 -0700

    Add preset vue/js files

---
+++ 16.Webpack/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <message>Erro ther</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: { Message },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
