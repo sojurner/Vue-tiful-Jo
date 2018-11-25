e02c8791ba52a46772f02aeb5518d81c7591b424
sojurner <pykim0591@gmail.com>
2018-11-25 06:39:19 -0700

    Create Message component

---
+++ 16.Webpack/src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+  
+<div class="box is-info">
+<p>
+  <slot></slot>
+</p>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.box {
+  background: gray;
+}
+</style>
